ee1c7baa28c32dabc4b42fd0ccb0cbfecd3ef4ce
Timo Kramer <fw-github@timokramer.de>
2023-05-01 11:39:41 +0200

    feat: implemented konserve-cassandra interface

---
+++ src/datahike_cassandra/core.clj
@@ -0,0 +1,35 @@
+(ns datahike-cassandra.core
+  (:require [datahike.store :refer [empty-store delete-store connect-store default-config config-spec release-store store-identity]]
+            [datahike.config :refer [map-from-env]]
+            [konserve-cassandra.core :as k]
+            [clojure.spec.alpha :as s]
+            [taoensso.timbre :as log])
+  (:import [com.datastax.oss.driver.api.core.servererrors InvalidQueryException]))
+
+(defmethod store-identity :cassandra [store-config]
+  (let [{:keys [session-keyspace contact-points]} store-config]
+      [:cassandra session-keyspace contact-points]))
+
+(defmethod empty-store :cassandra [store-config]
+  (k/connect-store (dissoc store-config :backend)))
+
+(defmethod delete-store :cassandra [store-config]
+  (try
+    (k/delete-store (dissoc store-config :backend))
+    (catch Exception _e)))
+
+(defmethod connect-store :cassandra [store-config]
+  (k/connect-store (dissoc store-config :backend)))
+
+(defmethod default-config :cassandra [config]
+  (merge
+   (map-from-env :datahike-store-config {:session-keyspace "konserve"})
+   config))
+
+(s/def :datahike.store.cassandra/session-keyspace string?)
+(s/def ::cassandra (s/keys :req-un [:datahike.store.cassandra/session-keyspace]))
+
+(defmethod config-spec :cassandra [_] ::cassandra)
+
+(defmethod release-store :cassandra [_ store]
+  (k/release store {:sync? true}))
---
+++ test/datahike_cassandra/core_test.clj
@@ -0,0 +1,18 @@
+(ns datahike-cassandra.core-test
+ (:require [clojure.test :as t :refer [deftest use-fixtures]]
+           [datahike.integration-test :refer [integration-test integration-test-fixture]]
+           [datahike-cassandra.core]))
+
+(def config {:store {:backend :cassandra
+                     :session-keyspace "konserve_test"}
+             :schema-flexibility :write
+             :keep-history? true})
+
+(defn fixture [f]
+  (integration-test-fixture config)
+  (f))
+
+(use-fixtures :once fixture)
+
+(deftest  datahike-integration-test
+  (integration-test config))
